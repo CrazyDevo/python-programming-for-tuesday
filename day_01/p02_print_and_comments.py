324402fec0385d2b01b055d750ab1d2d07769474
AdamB25 <barsialtunit@gmail.com>
2024-12-24 16:57:27 -0500

    Initial commit

---
+++ day_01/p02_print_and_comments.py
@@ -0,0 +1,11 @@
+# in order to print sth in the output we can use print
+print("Hello Adam")
+
+"""
+WE can use multiple line comments
+with 3 "  
+like this
+"""
+
+print("Goodbye", "Adam", "Alice")
+print("Hello Faranak")
